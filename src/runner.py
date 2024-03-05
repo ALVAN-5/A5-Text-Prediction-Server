3f5b0e781cf7df951ac694113277186f1a299e9c
Craig Sirota <34724663+csirota97@users.noreply.github.com>
2024-03-04 21:34:17 -0500

    Created docker infrastructure (#8)
    * Created dockerfile
    
    * wrote docker-compose
    
    ---------
    
    Co-authored-by: csirota97 <csirota97@gmail.com>

--- src/runner.py
+++ src/runner.py
@@ -5,8 +5,8 @@ import os
 def run(app):
     if os.environ['A5TPS_ENV'] == 'DEV':
         # Development
-        # app.run(host="192.168.1.2")
-        app.run(host="localhost")
+        # app.run(host="192.168.0.2", debug=True)
+        app.run(host="localhost", debug=True)
     elif os.environ['A5TPS_ENV'] == 'PROD':
         # Production
         http_server = WSGIServer((os.environ['A5TPS_HOST_IP'], int(os.environ['A5TPS_HOST_PORT'])), app)
